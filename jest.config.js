8eff7edf5f16ba4bba6818a2a7610430ca20ce9b
hankei6km <gh-hankei6km@outlook.jp>
2021-10-18 12:33:57 +0900

    Setup to native ESM

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
-module.exports = {
+export default {
   roots: ['<rootDir>'],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
     '**/?(*.)+(spec|test).+(ts|tsx|js)'
   ],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
+  transform: {},
+  // https://kulshekhar.github.io/ts-jest/docs/next/guides/esm-support/
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
   }
 };
